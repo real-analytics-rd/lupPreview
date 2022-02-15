9a44317328224bec2c95ad87abe0d0b8f2697641
MeherKharbachi <mkharbachi@ats-digital.com>
2022-02-15 16:50:24 +0100

    Prod script + some changes

--- scripts/prod_script.py
+++ scripts/prod_script.py
@@ -1,19 +1,27 @@
 import argparse
 import logging
-import pandas as pd
 import random
+import dateparser
+import sys
+import os
+import pandas as pd
 from time import sleep
+package_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(package_dir))
 from guardian_scraper.scraper import *
 from guardian_scraper.parser import *
 from guardian_scraper.Db_Config import *
 from guardian_scraper.models.preview import *
 
 
-def main(page, api_key, scraping_all):
-    # store scraper actions in a log file
-    logging.basicConfig(
-        filename="scraper.log", level=logging.INFO, format="%(levelname)s:%(message)s"
-    )
+def load_file(file_path):
+    # charging the file content in a dataframe
+    df = pd.read_csv(file_path)
+    return df
+
+
+def main(page, api_key, scraping_all, file_path):
+
     # starting url
     url = "https://www.theguardian.com/football/series/match-previews?page=" + str(page)
     # initialize the scraper instance.
@@ -35,11 +43,12 @@ def main(page, api_key, scraping_all):
         if previews_last_date != None:
             last_previews_date = previews_last_date.previewDate
 
+    logging.info("The scraper started: {}".format(dateparser.parse("now")))
     logging.info(
         "The last preview date stored in the database is: {}".format(last_previews_date)
     )
-    # charging teams dictionary
-    df_teams = pd.read_csv(".//datasets//final_data.csv")
+    # charging english teams file which contains teams and their different names
+    df_teams = load_file(file_path)
     # if we are not at the last page
     # and we haven't reached an extracted preview
     # we launch the scraper
@@ -63,20 +72,45 @@ def main(page, api_key, scraping_all):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="The Guardian Scraper")
-    parser.add_argument("page", metavar="page", type=int, help="Enter the page number")
     parser.add_argument(
-        "api_key", metavar="api_key", type=str, help="Enter your Guardian api key"
+        "--page", metavar="page", type=int, default=1, help="Enter the page number"
+    )
+    parser.add_argument(
+        "--api_key",
+        metavar="api_key",
+        type=str,
+        default="fd4452e9-76a5-45a1-b30d-bdd156640b9c",
+        help="Enter your Guardian api key",
     )
     parser.add_argument(
         "--scraping",
         metavar="scraping",
         type=str,
+        default=None,
         choices=["all"],
         help="Ignore the last preview date in Opta Database and scrape all previews",
     )
+    parser.add_argument(
+        "--file_path",
+        metavar="file_path",
+        type=str,
+        default="..//teams_mapping_datasets//english_teams.csv",
+        help="Enter the path of the teams mapping file",
+    )
     args = parser.parse_args()
-
+    # page number input
     page = args.page
-    api_key = args.api_key  # fd4452e9-76a5-45a1-b30d-bdd156640b9c
+    # api key input
+    api_key = args.api_key
+    # scraping method input
     scraping_all = args.scraping
-    main(page, api_key, scraping_all)
+    # teams mapping file path
+    file_path = args.file_path
+    # store scraper actions in a log file
+    logging.basicConfig(
+        filename="..//logs//scraper.log",
+        level=logging.INFO,
+        format="%(levelname)s:%(message)s",
+    )
+    # launch the scraper
+    main(page, api_key, scraping_all, file_path)
